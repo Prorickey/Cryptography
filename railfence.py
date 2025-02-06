f2b62d6f21fac0343f94c5bf0a822f7d25b6b8ba
Trevor Bedson <78997566+Prorickey@users.noreply.github.com>
2025-02-06 09:04:34 -0500

    Railfence stuff

---
+++ railfence.py
@@ -0,0 +1,82 @@
+def rail_fence_encode(text, n):
+    rows = []
+    for i in range(n):
+        rows.append("")
+
+    rn = 0
+    desc = False
+    for i in range(len(text)):
+        rows[rn] += text[i]
+        if desc:
+            if rn > 0:
+                rn -= 1
+            else:
+                desc = False
+                rn += 1
+        else:
+            if rn + 1 < n:
+                rn += 1
+            else:
+                desc = True
+                rn -= 1
+
+    ciphertext = ""
+    for row in rows:
+        ciphertext += row
+
+    return ciphertext.upper()
+
+def rail_fence_decode(ciphertext, n):
+    rail_map = [['\n' for _ in range(len(ciphertext))] for _ in range(n)]
+
+    rn = 0
+    desc = False
+
+    for i in range(len(ciphertext)):
+        rail_map[rn][i] = '*'
+        if desc:
+            if rn > 0:
+                rn -= 1
+            else:
+                desc = False
+                rn += 1
+        else:
+            if rn + 1 < n:
+                rn += 1
+            else:
+                desc = True
+                rn -= 1
+
+    index = 0
+    for r in range(n):
+        for c in range(len(ciphertext)):
+            if rail_map[r][c] == '*' and index < len(ciphertext):
+                rail_map[r][c] = ciphertext[index]
+                index += 1
+
+    plaintext = ""
+    rn = 0
+    desc = False
+
+    for i in range(len(ciphertext)):
+        plaintext += rail_map[rn][i]
+        if desc:
+            if rn > 0:
+                rn -= 1
+            else:
+                desc = False
+                rn += 1
+        else:
+            if rn + 1 < n:
+                rn += 1
+            else:
+                desc = True
+                rn -= 1
+
+    return plaintext
+
+
+test = "FWYREILEL"
+print(rail_fence_decode(test, 3))
+print(rail_fence_decode(test, 4))
+
