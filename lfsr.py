a4f849793e69d34b552a3ed0feb5811a4a514b59
Trevor Bedson <trevobedson@gmail.com>
2025-04-24 09:23:35 -0400

    Started on playfair cipher

--- lfsr.py
+++ lfsr.py
@@ -1,13 +1,23 @@
-def lfsr(bits: list, res: list) -> str:
-    xor = bits[1] ^ bits[4]
-    print(xor)
-    next1 = [xor] + bits[:-1]
-    res.append(bits[-1])
-    print(bits)
-    if len(res) == 12:
-        res = [str(x) for x in res]
-        return ''.join(res)
-    else:
-        return lfsr(next1, res)
+def get_bit(value, bit_index):
+    return (value >> (bit_index-1)) & 1
 
-print(lfsr([1, 1, 0, 1, 1, 0], []))
+def lfsr8(seed, num_bits):
+    """
+            8 7 6 5 4 3 2 1 (bits == 8)
+           ┌─┬─┬─┬─┬─┬─┬─┬─┐
+        ┌─→│1│0│1│0│0│0│0│1├─→ output
+        │  └─┴─┴─┴┬┴┬┴┬┴─┴┬┘
+        └──────XOR┘ │ │   │
+                └─XOR └─┐ │
+                    └──XOR┘
+                          (taps == 5, 4, 3, 1)
+    """
+    current_state = seed
+    output_stream = 0
+    
+    for i in range(num_bits):
+        output_stream = (output_stream << 1) | get_bit(current_state, 1)
+        xor = (((get_bit(current_state, 1) ^ get_bit(current_state, 3)) ^ get_bit(current_state, 4)) ^ get_bit(current_state, 5))
+        current_state = (current_state >> 1) | (xor << 7)
+        
+    return output_stream
